843ecfbd20072401516dbee7a3abccd5baa32ca4
Shiran <dudy@ohsu.edu>
2017-07-28 10:54:17 -0700

    confirming expected intersection outcome in unitTest

--- unitTest/specializer.py
+++ unitTest/specializer.py
@@ -6,8 +6,11 @@ import specializer
 
 one = fst.Fst.read("one.fst")
 sigfst = fst.Fst.read("sigma.fst")
+sigout = fst.Fst.read("sigout.fst")
 rhofst = fst.Fst.read("rho.fst")
+rhoout = fst.Fst.read("rhoout.fst")
 phifst = fst.Fst.read("phi.fst")
+phiout = fst.Fst.read("phiout.fst")
 
 
 sigma_label = 5
@@ -24,11 +27,12 @@ phifst = specializer.phi(phifst, phi_label, rewrite_mode, phi_self_loop).get()
 
 print "Orignial Fst"
 print one
-print "intersection with the sigma machine"
+print r'intersection with the sigma machine'
 print fst.intersect(sigfst, one)
+print fst.equal(fst.intersect(sigfst, one), sigout)
 print "intersection with the rho machine"
 print fst.intersect(rhofst, one)
+print fst.equal(fst.intersect(rhofst, one), rhoout)
 print "intersection with the phi machine"
 print fst.intersect(phifst, one)
-
-
+print fst.equal(fst.intersect(phifst, one), phiout)
--- unitTest/test_specializer.sh
+++ unitTest/test_specializer.sh
@@ -4,6 +4,9 @@ set -x
 
 fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols one.txt | fstarcsort > one.fst
 fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols sigma.txt | fstarcsort > sigma.fst
+fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols sigout.txt | fstarcsort > sigout.fst
 fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols rho.txt | fstarcsort > rho.fst
+fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols rhoout.txt | fstarcsort > rhoout.fst
 fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols phi.txt | fstarcsort > phi.fst
+fstcompile --isymbols=syms --osymbols=syms --keep_isymbols --keep_osymbols phiout.txt | fstarcsort > phiout.fst
 python specializer.py
